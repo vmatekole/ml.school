2963f4a829599379cb241790032f549cb2309fb4
Victor Matekole <victor@matekole.com>
2023-10-09 11:09:31 +0200

    update constants

--- penguins/code/constants.py
+++ penguins/code/constants.py
@@ -9,8 +9,8 @@ S3_LOCATION = f"s3://{BUCKET}/penguins"
 DATA_FILEPATH = Path().resolve() / "data.csv"
 
 
-# sagemaker_client = boto3.client("sagemaker")
-# iam_client = boto3.client("iam")
-# role = sagemaker.get_execution_role()
-# region = boto3.Session().region_name
-# sagemaker_session = sagemaker.session.Session()
+sagemaker_client = boto3.client("sagemaker")
+iam_client = boto3.client("iam")
+role = sagemaker.get_execution_role()
+region = boto3.Session().region_name
+sagemaker_session = sagemaker.session.Session()
