07a43ee7d770f3095a9e22b7a2bd91abd4d5f26f
Victor Matekole <victor@matekole.com>
2023-10-09 11:01:32 +0200

    clean up

---
+++ penguins/code/constants.py
@@ -0,0 +1,16 @@
+
+import boto3
+import sagemaker
+from pathlib import Path
+
+
+BUCKET = "vmate-mlschool3"
+S3_LOCATION = f"s3://{BUCKET}/penguins"
+DATA_FILEPATH = Path().resolve() / "data.csv"
+
+
+# sagemaker_client = boto3.client("sagemaker")
+# iam_client = boto3.client("iam")
+# role = sagemaker.get_execution_role()
+# region = boto3.Session().region_name
+# sagemaker_session = sagemaker.session.Session()
